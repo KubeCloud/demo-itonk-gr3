5b9fb06233f013f9a84444abeddaec3bcc9658ad
Kasper Nissen <kni@it-minds.dk>
2016-06-08 15:18:45 +0200

    script

--- run-demo-itonk-grp3.sh
+++ run-demo-itonk-grp3.sh
@@ -15,5 +15,35 @@ kubectl create -f kubernetes-yaml/gateway-service-svc.yaml
 sleep 20s
 echo '<- API Gateway started'
 
+echo '-> Starting Product-service...'
+kubectl create -f kubernetes-yaml/product-service-deployment.yaml
+kubectl create -f kubernetes-yaml/product-service-svc.yaml
+
+sleep 20s
+echo '<- Product-service started'
+
+
+echo '-> Starting Order-service...'
+kubectl create -f kubernetes-yaml/order-service-deployment.yaml
+kubectl create -f kubernetes-yaml/order-service-svc.yaml
+
+sleep 20s
+echo '<- Order-service started'
+
+
+echo '-> Starting Web-service...'
+kubectl create -f kubernetes-yaml/web-service-deployment.yaml
+kubectl create -f kubernetes-yaml/web-service-svc.yaml
+
+sleep 20s
+echo '<- Web-service started'
+
+
+echo '-> Starting Website...'
+kubectl create -f kubernetes-yaml/website-deployment.yaml
+kubectl create -f kubernetes-yaml/website-svc.yaml
+
+sleep 20s
+echo '<- Website started'
 
 
